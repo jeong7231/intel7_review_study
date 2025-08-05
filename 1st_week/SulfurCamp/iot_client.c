7f81e146f6587cd8b5008fc9480f4c38a195eb6c
GaSap58 <isdnket@naver.com>
2025-08-05 17:58:28 +0900

    0805 TCP/IP 공부 계정관리(DB활용), 메시지파싱

--- 1st_week/SulfurCamp/iot_client.c
+++ 1st_week/SulfurCamp/iot_client.c
@@ -1,5 +1,6 @@
 /* 서울기술 교육센터 IoT */
 /* author : HJY + DB 개량 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,10 +15,12 @@
 #define NAME_SIZE 20
 #define ARR_CNT 5
 
+// 송신 및 수신 스레드 함수 선언
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
 
+// 전역 변수 (클라이언트 이름 및 송신 메시지 버퍼)
 char name[NAME_SIZE] = "[Default]";
 char msg[BUF_SIZE];
 
@@ -28,6 +31,7 @@ int main(int argc, char *argv[])
     pthread_t snd_thread, rcv_thread;
     void * thread_return;
 
+    // 인자 확인: IP, 포트, 이름
     if(argc != 4) {
         printf("Usage : %s <IP> <port> <name>\n", argv[0]);
         exit(1);
@@ -40,30 +44,36 @@ int main(int argc, char *argv[])
     printf("Password for [%s]: ", name);
     scanf("%s", passwd);
 
+    // 소켓 생성
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock == -1)
         error_handling("socket() error");
 
+    // 서버 주소 설정
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
+    // 서버 연결 요청
     if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error");
 
-    // 로그인 정보 전송
+    // 로그인 정보 전송 (id:pw)
     sprintf(msg, "%s:%s", name, passwd);
     write(sock, msg, strlen(msg));
 
+    // 메시지 수신 및 송신용 스레드 생성
     pthread_create(&rcv_thread, NULL, recv_msg, (void *)&sock);
     pthread_create(&snd_thread, NULL, send_msg, (void *)&sock);
 
+    // 송신 스레드 종료 시 메인도 종료
     pthread_join(snd_thread, &thread_return);
     close(sock);
     return 0;
 }
 
+// 메시지 송신 스레드
 void * send_msg(void * arg)
 {
     int *sock = (int *)arg;
@@ -87,14 +97,20 @@ void * send_msg(void * arg)
         if(FD_ISSET(STDIN_FILENO, &newset))
         {
             fgets(msg, BUF_SIZE, stdin);
-            if(!strncmp(msg,"quit\n",5)) {
+
+            if(!strncmp(msg, "quit\n", 5)) {
                 *sock = -1;
                 return NULL;
             }
-            else if(msg[0] != '[') {
+
+            //메시지 유효성 검사
+            if (strspn(msg, "\r\n") == strlen(msg)) continue; // 줄바꿈만 있을 경우 무시
+
+            if(msg[0] != '[') {
                 strcat(name_msg, "[ALLMSG]");
                 strcat(name_msg, msg);
             } else {
+                if (strlen(msg) < 5 || strchr(msg, ']') == NULL) continue; // 잘못된 형식 무시
                 strcpy(name_msg, msg);
             }
 
@@ -108,6 +124,8 @@ void * send_msg(void * arg)
     }
 }
 
+
+// 메시지 수신 스레드
 void * recv_msg(void * arg)
 {
     int * sock = (int *)arg;
@@ -123,11 +141,26 @@ void * recv_msg(void * arg)
         }
 
         name_msg[str_len] = '\0';
-        fputs(name_msg, stdout);   //  줄바꿈 없는 그대로 출력
-        fflush(stdout);            //  즉시 출력
+
+        // 빈 문자열, 공백, 줄바꿈만 있는 메시지 무시
+        if (strspn(name_msg, "\r\n") == str_len) continue;
+
+        // 삭제, 비밀번호변경 메시지 수신 시 종료 처리
+        if (strstr(name_msg, "You have been deleted") != NULL ||
+            strstr(name_msg, "Your password has been changed") != NULL) {
+            fputs(name_msg, stdout);
+            fflush(stdout);
+            *sock = -1;
+            close(*sock);
+            exit(0);  // 즉시 종료
+        }
+
+        fputs(name_msg, stdout);
+        fflush(stdout);
     }
 }
 
+// 에러 처리 함수
 void error_handling(char * msg)
 {
     fputs(msg, stderr);
